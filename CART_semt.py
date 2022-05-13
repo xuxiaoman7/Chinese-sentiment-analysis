b605442b16b538efc8019831ed0b2e964b11e2a9
xuxiaoman7 <xuxiaoman7@163.com>
2022-05-13 23:07:21 +0800

    CART update

--- CART_semt.py
+++ CART_semt.py
@@ -8,6 +8,8 @@ import joblib
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
+import matplotlib .pyplot as plt
+from sklearn.tree import plot_tree
 
 #CART模型
 #导入词向量作为训练特征
@@ -15,18 +17,22 @@ x = np.load('train_x_vec.npy')
 #导入情绪分类作为目标特征
 y = np.load('train_y.npy')
 
-model = DecisionTreeClassifier()
+#设置最大深度，深度过深容易过拟合
+model = DecisionTreeClassifier(max_depth=20)
 
 #采用10折交叉验证法训练模型
 kf = KFold(n_splits=10,shuffle=True,random_state=0)
 curr_acc = 0
 for train_index,test_index in kf.split(x):
-    #print(train_index.shape)             #(1260,)
     clt = model.fit(x[train_index],y[train_index])
     y_predict = model.predict(x[test_index])
     curr_acc = curr_acc + accuracy_score(y_predict,y[test_index])
-    #print(clt.score(x[test_index],y[test_index]))
 print("模型准确率为:",curr_acc/10)
+
+#数据可视化
+plt.figure()
+plot_tree(model,filled=True)
+plt.show()
 
 #保存模型为二进制文件
 joblib.dump(model,'dt_model.pkl')
--- ProcessData.py
+++ ProcessData.py
@@ -25,21 +25,19 @@ for i in range(0,2000):
     words = jieba.lcut(str)
     list_x.append(words)
 #训练word2vec模型(求词向量）
-model = Word2Vec(list_x,sg=1,vector_size=200,window=5,min_count=2,negative=3,sample=0.001,hs=1,workers=4)
+#因为样本数不多，所以特征的维度也小一点 100
+model = Word2Vec(list_x,sg=1,vector_size=100,window=5,min_count=2,negative=3,sample=0.001,hs=1,workers=4)
 model.save("word2vec_model.pkl")
 
 #求句向量
 def sum_vec(text):
-    vec = np.zeros(200).reshape((1,200))
+    vec = np.zeros(100).reshape((1,100))
     for word in text:
         try:
-            vec += model.wv[word].reshape((1,200))
+            vec += model.wv[word].reshape((1,100))
         except KeyError:
             continue
     return vec
 
 train_vec = np.concatenate([sum_vec(z) for z in list_x])
-print(train_vec.shape)
-train_vec = train_vec.reshape((1400,200))
 np.save('train_x_vec.npy',train_vec)
-print(train_vec.shape)
